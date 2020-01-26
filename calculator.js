668b44daf330430b9a50d1e5b2650a1685b082ba
Fermento99 <pk.pawlak99@gmail.com>
2020-01-26 21:43:28 +0100

    rewrite functions to apply convention

--- calculator.js
+++ calculator.js
@@ -16,8 +16,6 @@ function pow(a, c) {
 
 function chances(life, dice, success) {
     let outcome = 0
-    dice++
-    dice--
     for (let i = life; i <= dice; i++) {
         outcome += newton(dice, i) * Math.pow(success, i) * Math.pow(1 - success, dice - i)
     }
@@ -35,18 +33,18 @@ function checkRadio() {
 
 function checkLife() {
     const v = document.getElementById("life").value
-    return v
+    return v*1
 }
 
 function checkDice() {
     const v = document.getElementById("dice").value
-    return v
+    return v*1
 }
 
 document.getElementById("accept").onclick = (ev) => {
     let s = checkRadio()
     let l = checkLife()
     let d = checkDice()
-    console.log("chance: " + Math.round(s*100) + "%, life: " + l + ", dice: " + d)
+    console.log("life: " + l + ", dice: " + d + ", chance: " + s)
     document.getElementById("output-line").innerHTML = "Sznasa na sukces:  <b>" + chances(l, d, s) + "</b>"
 }
