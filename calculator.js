a767028c77ceccbf22c814345198fd88761542ba
Fermento99 <pk.pawlak99@gmail.com>
2020-01-26 22:35:55 +0100

    rewrite functions to apply convention

--- calculator.js
+++ calculator.js
@@ -1,12 +1,12 @@
 
 //  ########## MATH ########## 
-function newton(a, c) {
+const newton = (a, c) => {
     if (c < 1)
         return 1
     return newton(a - 1, c - 1) * (a / c)
 }
 
-function pow(a, c) {
+const pow = (a, c) => {
     const b = a
     for (; c > 1; c--) {
         a *= b
@@ -14,7 +14,7 @@ function pow(a, c) {
     return a;
 }
 
-function chances(life, dice, success) {
+const chances = (life, dice, success) => {
     let outcome = 0
     for (let i = life; i <= dice; i++) {
         outcome += newton(dice, i) * Math.pow(success, i) * Math.pow(1 - success, dice - i)
@@ -24,19 +24,19 @@ function chances(life, dice, success) {
 
 // ########## HANDLERS ##########
 
-function checkRadio() {
+const checkRadio = () => {
     if(document.getElementById("normal").checked == true) return 1/3
     if(document.getElementById("blessed").checked == true) return 1/2
     if(document.getElementById("cursed").checked == true) return 1/6
     return 0
 }
 
-function checkLife() {
+const checkLife = () => {
     const v = document.getElementById("life").value
     return v*1
 }
 
-function checkDice() {
+const checkDice = () => {
     const v = document.getElementById("dice").value
     return v*1
 }
